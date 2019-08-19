2709212456f49d32a656de14bea798d5aa73a413
nak-244 <t09024656860@gmail.com>
2019-08-19 17:20:54 +0900

    up

--- docs/ver3/js/box-job.js
+++ docs/ver3/js/box-job.js
@@ -3,71 +3,71 @@ $('#srh_jobtype_param').change(function() {
   var val = $(this).val();
   switch (val) {
     case '1':
-    $('#box1').show();
-    $('div[id^="box"]:not("#box1")').hide();
+    $('#box-job1').show();
+    $('div[id^="box-job"]:not("#box-job1")').hide();
     break;
     case '2':
-    $('#box2').show();
-    $('div[id^="box"]:not("#box2")').hide();
+    $('#box-job2').show();
+    $('div[id^="box-job"]:not("#box-job2")').hide();
     break;
     case '3':
-    $('#box3').show();
-    $('div[id^="box"]:not("#box3")').hide();
+    $('#box-job3').show();
+    $('div[id^="box-job"]:not("#box-job3")').hide();
     break;
     case '4':
-    $('#box4').show();
-    $('div[id^="box"]:not("#box4")').hide();
+    $('#box-job4').show();
+    $('div[id^="box-job"]:not("#box-job4")').hide();
     break;
     case '5':
-    $('#box5').show();
-    $('div[id^="box"]:not("#box5")').hide();
+    $('#box-job5').show();
+    $('div[id^="box-job"]:not("#box-job5")').hide();
     break;
     case '6':
-    $('#box6').show();
-    $('div[id^="box"]:not("#box6")').hide();
+    $('#box-job6').show();
+    $('div[id^="box-job"]:not("#box-job6")').hide();
     break;
     case '7':
-    $('#box7').show();
-    $('div[id^="box"]:not("#box7")').hide();
+    $('#box-job7').show();
+    $('div[id^="box-job"]:not("#box-job7")').hide();
     break;
     case '8':
-    $('#box8').show();
-    $('div[id^="box"]:not("#box8")').hide();
+    $('#box-job8').show();
+    $('div[id^="box-job"]:not("#box-job8")').hide();
     break;
     case '9':
-    $('#box9').show();
-    $('div[id^="box"]:not("#box9")').hide();
+    $('#box-job9').show();
+    $('div[id^="box-job"]:not("#box-job9")').hide();
     break;
     case '10':
-    $('#box10').show();
-    $('div[id^="box"]:not("#box10")').hide();
+    $('#box-job10').show();
+    $('div[id^="box-job"]:not("#box-job10")').hide();
     break;
     case '11':
-    $('#box11').show();
-    $('div[id^="box"]:not("#box11")').hide();
+    $('#box-job11').show();
+    $('div[id^="box-job"]:not("#box-job11")').hide();
     break;
     case '12':
-    $('#box12').show();
-    $('div[id^="box"]:not("#box12")').hide();
+    $('#box-job12').show();
+    $('div[id^="box-job"]:not("#box-job12")').hide();
     break;
     case '13':
-    $('#box13').show();
-    $('div[id^="box"]:not("#box13")').hide();
+    $('#box-job13').show();
+    $('div[id^="box-job"]:not("#box-job13")').hide();
     break;
     case '14':
-    $('#box14').show();
-    $('div[id^="box"]:not("#box14")').hide();
+    $('#box-job14').show();
+    $('div[id^="box-job"]:not("#box-job14")').hide();
     break;
     case '15':
-    $('#box15').show();
-    $('div[id^="box"]:not("#box15")').hide();
+    $('#box-job15').show();
+    $('div[id^="box-job"]:not("#box-job15")').hide();
     break;
     case '16':
-    $('#box16').show();
-    $('div[id^="box"]:not("#box16")').hide();
+    $('#box-job16').show();
+    $('div[id^="box-job"]:not("#box-job16")').hide();
     break;
     default:
-    $('div[id^="box"]').hide();
+    $('div[id^="box-job"]').hide();
   }
 });
 });
